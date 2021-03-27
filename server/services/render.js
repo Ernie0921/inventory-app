61a4d62739ebbdc240a954752736e536089e0a81
Ernesto Carrillo <ernesto.carrillo10@icloud.com>
2021-03-27 03:26:45 -0400

    added update, delte actions

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,19 @@
+const axios = require('axios');
+
 exports.homeRoutes = (req, res) => {  // render the index ejs and import it 
-    res.render('index');
+
+    //make a get request to /api/users
+    axios.get('http://localhost:3000/api/users')
+    .then(function(response){
+        
+
+        res.render('index',{ users: response.data})
+    })
+    .catch(err =>{
+        res.send(err)
+    })
+
+    
 }
 
 
@@ -9,5 +23,12 @@ exports.add_user = (req, res) => {
 
 
 exports.update_user = (req, res) =>{
-    res.render('update_user');
+
+    axios.get('http://localhost:3000/api/users',{ params: { id: req.query.id}})
+    .then(function(userdata){
+        res.render("update_user", { user: userdata.data})
+    })
+    .catch(err => {
+        res.send(err)
+    })
 }
